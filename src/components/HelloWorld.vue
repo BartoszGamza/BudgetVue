e6af6499469a0ddaf1e40000ca4cdb68dbaf5af6
bartoszgamza <bartosz.gamza12@gmail.com>
2018-04-01 17:49:23 +0200

    added table and button

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,35 +1,30 @@
 <template>
-  <v-container fluid>
-    <v-slide-y-transition mode="out-in">
-      <v-layout column align-center>
-        <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
-        <blockquote>
-          &#8220;First, solve the problem. Then, write the code.&#8221;
-          <footer>
-            <small>
-              <em>&mdash;John Johnson</em>
-            </small>
-          </footer>
-        </blockquote>
-      </v-layout>
-    </v-slide-y-transition>
-  </v-container>
+  <div>
+  <v-expansion-panel expand>
+    <v-expansion-panel-content v-for="(item,i) in 5" :key="i" :value="item === 2">
+      <div slot="header">Item</div>
+      <v-card>
+        <v-card-text class="grey lighten-3">Item Details</v-card-text>
+        <v-card-actions>
+          <v-btn flat color="green">Edit</v-btn>
+          <v-btn flat color="red">Delete</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-expansion-panel-content>
+  </v-expansion-panel>
+  <v-btn
+              fixed
+              dark
+              fab
+              bottom
+              right
+              color="pink"
+            >
+              <v-icon>add</v-icon>
+            </v-btn>
+  </div>
 </template>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<script>
+
+</script>
