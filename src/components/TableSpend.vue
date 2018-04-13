67c8d092bc9026e3327bd0f9685126fa90b018a2
bartoszgamza <bartosz.gamza12@gmail.com>
2018-04-13 18:54:49 +0200

    adding to temp list, remove and edit to be fixed

--- src/components/TableSpend.vue
+++ src/components/TableSpend.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
     <v-expansion-panel expand>
-    <v-expansion-panel-content v-for="(item,i) in 5" :key="i" :value="item === 2">
-      <div slot="header">Item</div>
+    <v-expansion-panel-content v-for="item in items" :key="item.key">
+      <div slot="header">{{item.amnt}}</div>
       <v-card>
-        <v-card-text class="grey lighten-3">Item Details</v-card-text>
+        <v-card-text class="grey lighten-3">{{item.desc}}</v-card-text>
         <v-card-actions>
           <v-btn flat color="green">Edit</v-btn>
-          <v-btn flat color="red">Delete</v-btn>
+          <v-btn flat color="red" @click="deleteItem(item.key)">Delete</v-btn>
         </v-card-actions>
       </v-card>
     </v-expansion-panel-content>
@@ -27,15 +27,16 @@
         <v-card>
           <v-form>
         <v-text-field
-        label="Email"
+        label="Amnout"
+        v-model="amnout"
         ></v-text-field>
         <v-text-field
-        label="Password"
-        type="password"
+        label="Name"
+        v-model="description"
      ></v-text-field>
    </v-form>
     <v-card-actions>
-      <v-btn color="primary">Save</v-btn>
+      <v-btn color="primary" @click="saveItem">Save</v-btn>
       <v-btn @click.stop="dialog = !dialog">Cancel</v-btn> 
     </v-card-actions>
         </v-card>
@@ -48,8 +49,22 @@ export default {
   name: 'TableSpend',
   data () {
     return {
-      dialog: false
+      dialog: false,
+      items: [],
+      amnout: '',
+      description: ''
     }
+  },
+  methods: {
+    saveItem () {
+      this.items.push({amnt: this.amnout, desc: this.description})
+      this.dialog = !this.dialog
+    },
+    deleteItem (key) {
+      // this.items.splice(id, 1)
+      console.log(key)
+    },
+    editItem () {}
   }
 }
 </script>
