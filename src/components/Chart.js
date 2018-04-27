fa035e7a0b00ff76e8eba78a003fbaee9cfe9c0a
bartoszgamza <bartosz.gamza12@gmail.com>
2018-04-27 22:09:40 +0200

    data added to stats

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,17 +1,21 @@
-import { Bar } from 'vue-chartjs'
+import { Line } from 'vue-chartjs'
 
 export default {
-  extends: Bar,
+  extends: Line,
   mounted () {
     // Overwriting base render method with actual data.
+    const items = this.$store.getters.loadedItems
+    // console.log(items)
+    const times = items.map(function (item) { return item.date })
+    const vals = items.map(function (item) { return parseInt(item.amnt) })
     this.renderChart({
 
-      labels: [],
+      labels: times,
       datasets: [
         {
-          label: 'GitHub Commits',
-          backgroundColor: '#f87979',
-          data: []
+          label: 'Spending spree',
+          backgroundColor: '#fc005a',
+          data: vals
         }
       ]
     })
